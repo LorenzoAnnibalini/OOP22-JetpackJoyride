de968c05338895199a602cc8a328e25bbb591a08
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-03-29 13:50:51 +0200

    Refactoring of method downloadSaves and finished uploadSaves

--- src/main/java/it/unibo/jetpackjoyride/core/impl/Saves.java
+++ src/main/java/it/unibo/jetpackjoyride/core/impl/Saves.java
@@ -1,34 +1,81 @@
 package it.unibo.jetpackjoyride.core.impl;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
+
+import it.unibo.jetpackjoyride.model.impl.Deaths;
+import it.unibo.jetpackjoyride.model.impl.GrabbedMoney;
+import it.unibo.jetpackjoyride.model.impl.GrabbedObjects;
+import it.unibo.jetpackjoyride.model.impl.KilledNpc;
+import it.unibo.jetpackjoyride.model.impl.MaxMeters;
+import it.unibo.jetpackjoyride.model.impl.MaxMoney;
+import it.unibo.jetpackjoyride.model.impl.MoneySpent;
+import it.unibo.jetpackjoyride.model.impl.TotalMeters;
+
 import java.io.*;
 
-import it.unibo.jetpackjoyride.model.api.Statistics;
-import it.unibo.jetpackjoyride.model.impl.StatisticsImpl;
-
-public class Saves{
+/**
+ * Class to save statistics on csv file.
+ * 
+ * @author emanuele.sanchi@studio.unibo.it
+ */
+public class Saves {
 
     private final static String SEPARATOR = File.separator;
+    private static Deaths deaths;
+    private static KilledNpc killedNpc;
+    private static GrabbedMoney grabbedMoney;
+    private static GrabbedObjects grabbedObjects;
+    private static MaxMeters maxMeters;
+    private static MaxMoney maxMoney;
+    private static MoneySpent moneySpent;
+    private static TotalMeters totalMeters;
+    private final static String filename = "resources" + SEPARATOR + "saves.csv";
 
-    public static Statistics downloadSaves() throws FileNotFoundException {
-        Scanner sc = new Scanner(new File("resources"+SEPARATOR+"saves.csv"));
-        List<Integer> retValues = new ArrayList<>();
+    /**
+     * Method to download datas from csv file.
+     * 
+     * @return a map that maps every stat name with his value
+     * @throws FileNotFoundException
+     */
+    public static Map<String, Integer> downloadSaves() throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(filename));
+        Map<String, Integer> statistics = new HashMap<>();
         sc.useDelimiter(";");
-        sc.nextLine();//skip the first line of csv file because there are  the intestation of columns
-        while(sc.hasNext()) {
-            int value = Integer.parseInt(sc.next());
-            retValues.add(value);
+        while (sc.hasNextLine()) {
+            String name = sc.next();
+            int value = sc.nextInt();
+            statistics.put(name, value);
         }
-        sc.close(); 
-        Statistics statistics = new StatisticsImpl(retValues.get(0),retValues.get(1),retValues.get(0),retValues.get(0),retValues.get(0),retValues.get(0),retValues.get(0),retValues.get(0));
+        sc.close();
+        // Creation of statistics objects
+        deaths = new Deaths(statistics.get(statistics.keySet().stream().filter(x -> x == "Deaths").findFirst().get()));
+        grabbedMoney = new GrabbedMoney(statistics.get(statistics.keySet().stream().filter(x -> x == "GrabbedMoney").findFirst().get()));
+        grabbedObjects = new GrabbedObjects(statistics.get(statistics.keySet().stream().filter(x -> x == "GrabbedObjects").findFirst().get()));
+        killedNpc = new KilledNpc(statistics.get(statistics.keySet().stream().filter(x -> x == "KilledNpc").findFirst().get()));
+        maxMeters = new MaxMeters(statistics.get(statistics.keySet().stream().filter(x -> x == "MaxMeters").findFirst().get()));
+        maxMoney = new MaxMoney(statistics.get(statistics.keySet().stream().filter(x -> x == "MaxMoney").findFirst().get()));
+        moneySpent = new MoneySpent(statistics.get(statistics.keySet().stream().filter(x -> x == "MoneySpent").findFirst().get()));
+        totalMeters = new TotalMeters(statistics.get(statistics.keySet().stream().filter(x -> x == "TotalMeters").findFirst().get()));
         return statistics;
     }
 
-    public static void uploadSaves() {
-        //Statistics stats = new StatisticsImpl();
-
+    /**
+     * Method that writes new statistics on file.
+     * @throws IOException
+     */
+    public static void uploadSaves() throws IOException{
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
+        writer.write(deaths.getName() + ";" + deaths.getValue() + "\n");
+        writer.write(killedNpc.getName() + ";" + killedNpc.getValue() + "\n");
+        writer.write(grabbedMoney.getName() + ";" + grabbedMoney.getValue() + "\n");
+        writer.write(grabbedObjects.getName() + ";" + grabbedObjects.getValue() + "\n");
+        writer.write(maxMeters.getName() + ";" + maxMeters.getValue() + "\n");
+        writer.write(maxMoney.getName() + ";" + maxMoney.getValue() + "\n");
+        writer.write(moneySpent.getName() + ";" + moneySpent.getValue() + "\n");
+        writer.write(totalMeters.getName() + ";" + totalMeters.getValue() + "\n");
+        writer.close();
     }
-    
+
 }
