1210b3a332ab3b820b48b6621e1b6c7272087878
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-03-29 22:36:22 +0200

    Add set and get of the speed and the texture of the scientist

--- src/main/java/it/unibo/jetpackjoyride/model/api/Scientist.java
+++ src/main/java/it/unibo/jetpackjoyride/model/api/Scientist.java
@@ -1,7 +1,8 @@
 package it.unibo.jetpackjoyride.model.api;
 
 /**
- * TODO: Descrizione interfaccia
+ * TODO: Mettiamo la PATH della texture qui ?
+ * Interface for the scientist
  * @author lorenzo.annibalini@studio.unibo.it
  */
 
@@ -24,5 +25,32 @@ public interface Scientist {
      * Set the life of the scientist to false
      */
     public void killScientist();
+
+    /**
+     * Method to set the speed of the scientist
+     * @param speed the speed of the scientist
+     */
+    public void setSpeed(double speed);
+
+    /**
+     * Method to get the speed of the scientist
+     * @return the speed of the scientist
+     * @throws IllegalArgumentException if the speed is not allowed
+     */
+    public double getSpeed();
+
+    //TODO: vedi commento a inizio interfaccia
+    /**
+     * Method to get the texture of the scientist
+     * @return the texture of the scientist
+     */
+    public String getTexture();
+
+    //TODO: vedi commento a inizio interfaccia
+    /**
+     * Method to set the texture of the scientist
+     * @param texture the texture of the scientist
+     */
+    public void setTexture(String texture);
     
 }
--- src/main/java/it/unibo/jetpackjoyride/model/impl/ScientistImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/ScientistImpl.java
@@ -3,14 +3,25 @@ package it.unibo.jetpackjoyride.model.impl;
 import it.unibo.jetpackjoyride.model.api.Direction;
 import it.unibo.jetpackjoyride.model.api.Scientist;
 
+/**
+ * TODO: Vedi commento su texture nell'interfaccia ?
+ * Standard scientist implementation
+ * @author lorenzo.annibalini@studio.unibo.it
+ */
+
 public class ScientistImpl implements Scientist{
+
+    final static double MAXSPEED = 10;
 
     private final Direction direction;
     private boolean life;
+    private double speed;
+    private String texture;
 
-    public ScientistImpl(Direction direction) {
+    public ScientistImpl(Direction direction, String texture) {
         this.direction = direction;
         this.life = true;
+        this.texture = texture;
     }
 
     @Override
@@ -26,6 +37,29 @@ public class ScientistImpl implements Scientist{
     @Override
     public void killScientist() {
         this.life = false;    
+    }
+
+    @Override
+    public void setSpeed(double speed) {
+        if(speed < 0 && speed > MAXSPEED) {
+            throw new IllegalArgumentException("Speed can't be negative");
+        }
+        this.speed = speed;
+    }
+
+    @Override
+    public double getSpeed() {
+        return this.speed;
+    }
+
+    @Override
+    public String getTexture() {
+        return this.texture;
+    }
+
+    @Override
+    public void setTexture(String texture) {
+        this.texture = texture;
     }
     
 }
