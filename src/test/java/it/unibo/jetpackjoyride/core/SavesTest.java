9e071fae2df4a0dd871d8307be37ecb10aaac416
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-03-30 16:11:16 +0200

    Complete method uploadSaves

---
+++ src/test/java/it/unibo/jetpackjoyride/core/SavesTest.java
@@ -0,0 +1,13 @@
+package it.unibo.jetpackjoyride.core;
+
+import org.junit.jupiter.api.Test;
+
+public class SavesTest {
+    
+    /**
+     * Test for downloadSaves and uploadSaves
+     */
+    @Test
+    
+
+}
