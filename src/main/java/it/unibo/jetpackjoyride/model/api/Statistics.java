f9e8ebe5731914632cc466382eb68556294a6c60
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-03-30 11:26:57 +0200

    Add javadoc

--- src/main/java/it/unibo/jetpackjoyride/model/api/Statistics.java
+++ src/main/java/it/unibo/jetpackjoyride/model/api/Statistics.java
@@ -1,15 +1,32 @@
 package it.unibo.jetpackjoyride.model.api;
 
 /**
- * Interface of class to get, set and increment statistics
+ * Interface of class to get, set and increment statistics.
+ * 
+ * @author emanuele.sanchi@studio.unibo.it
  */
 public interface Statistics {
 
+    /**
+     * Getter method to take value of the statistic.
+     * @return value of statistic
+     */
     int getValue();
 
+    /**
+     * Setter method to set name of the statistic.
+     * @param amount value to set/add
+     */
     void setValue(final int amount);
 
+    /**
+     * Method to increment value of statistic.
+     */
     void increment();    
 
+    /**
+     * Getter method to take statistic's name.
+     * @return the name of the statistic
+     */
     String getName();
 }
