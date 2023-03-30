1b0b0b9b396ffd28744ff7b09af2fda040203c9a
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-03-30 16:35:10 +0200

    added Obstacles interface and GameObject class

---
+++ src/main/java/it/unibo/jetpackjoyride/model/api/Obstacles.java
@@ -0,0 +1,10 @@
+package it.unibo.jetpackjoyride.model.api;
+
+/**
+ * Interface that represents a generic obstacle.
+ * @author lorenzo.bacchini4@studio.unibo.it
+ */
+
+public interface Obstacles {
+    
+}
