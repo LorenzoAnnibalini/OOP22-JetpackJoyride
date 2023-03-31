391a261edf3ab187f86614efe100c8816b41c2dd
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-03-31 23:50:12 +0200

    Added obstacles class Electrode, LaserRay, Rocket

---
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/Electrode.java
@@ -0,0 +1,18 @@
+package it.unibo.jetpackjoyride.model.impl;
+
+import it.unibo.jetpackjoyride.common.Point2d;
+import it.unibo.jetpackjoyride.common.Vector2d;
+
+/**
+ * Class to model an Electrode obstacle.
+ * @author lorenzo.bacchini4@studio.unibo.it
+ */
+
+public class Electrode extends Obstacles{
+
+    public Electrode(Type type, Point2d pos, Vector2d vel) {
+        super(type, pos, vel);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/LaserRay.java
@@ -0,0 +1,18 @@
+package it.unibo.jetpackjoyride.model.impl;
+
+import it.unibo.jetpackjoyride.common.Point2d;
+import it.unibo.jetpackjoyride.common.Vector2d;
+
+/**
+ * Class to model a LaserRay obstacle.
+ * @author lorenzo.bacchini4@studio.unibo.it
+ */
+
+public class LaserRay extends Obstacles{
+
+    public LaserRay(Type type, Point2d pos, Vector2d vel) {
+        super(type, pos, vel);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
