fd5a055e3ebfb7078efcffeb1a10146417375a21
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-04-01 00:15:12 +0200

    added standard method to Obstacle

--- src/main/java/it/unibo/jetpackjoyride/model/impl/Obstacle.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/Obstacle.java
@@ -10,9 +10,38 @@ import it.unibo.jetpackjoyride.common.Vector2d;
 
 public abstract class Obstacle extends GameObject{
 
+    private boolean active;
+
+    /**
+     * Constructor of the Obstacle 
+     * that only recall the constructor of the superclass.
+     * @param type
+     * @param pos
+     * @param vel
+     */
     public Obstacle(Type type, Point2d pos, Vector2d vel) {
         super(type, pos, vel);
-        //TODO Auto-generated constructor stub
     }
     
+    /**
+     * Set the obstacle active = true.
+     */
+    public void setActiveOn(){
+        this.active = true;
+    }
+
+    /**
+     * Set the obstacle active = false.
+     */
+    public void setActiveOff(){
+        this.active = false;
+    }
+
+    /**
+     * Return true if the obstacle is active, false otherwise.
+     * @return true if obstacle is active, false otherwise
+     */
+    public boolean isActive(){
+        return this.active;
+    }
 }
