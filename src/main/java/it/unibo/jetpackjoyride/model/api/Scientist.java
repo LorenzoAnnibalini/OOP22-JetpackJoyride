0d45097f56c437ed4693f6bce58c7d7fd6bcecf4
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-04-01 12:29:19 +0200

    Deleted: -set/get Position and velocity in ScientistImpl and Interface -set/get og the texture array in ScientistImpl and Interface -run method in ScientistImpl and Interface Add: -nextPosition method in ScientistImpl and Interface as public

--- src/main/java/it/unibo/jetpackjoyride/model/api/Scientist.java
+++ src/main/java/it/unibo/jetpackjoyride/model/api/Scientist.java
@@ -1,10 +1,6 @@
 package it.unibo.jetpackjoyride.model.api;
 
-import it.unibo.jetpackjoyride.common.Point2d;
-import it.unibo.jetpackjoyride.common.Vector2d;
-
 /**
- * TODO: Mettiamo la PATH della texture qui ?
  * Interface for the scientist
  * @author lorenzo.annibalini@studio.unibo.it
  */
@@ -30,59 +26,11 @@ public interface Scientist {
     public void killScientist();
 
     /**
-     * Method to get the current position of the scientist
-     * @return the current position of the scientist
-     */
-    public Point2d getCurrentPos();
-
-    /**
-     * Method to set the current position of the scientist
-     * @param point the new position of the scientist
-     * @throws IllegalArgumentException if the point is null
-     */
-    public void setCurrentPos(Point2d point);
-
-    /**
-     * Method to get the current velocity of the scientist
-     * @return the current velocity of the scientist
-     */
-    public Vector2d getVelocity();
-
-    /**
-     * Method to set the current velocity of the scientist
-     * @param velocity the new velocity of the scientist
-     * @throws IllegalArgumentException if the velocity is null
-     */
-    public void setVelocity(Vector2d velocity);
-
-    //TODO: vedi commento a inizio interfaccia
-    /**
-     * Method to get the texture array of the scientist
-     * @return the texture array of the scientist
-     */
-    public String[] getTextureArray();
-
-    /**
-     * Method to get the current texture of the scientist
-     * @return the current texture of the scientist
-     */
-    public String getCurrentTexture();
-
-    //TODO: vedi commento a inizio interfaccia
-    /**
-     * Method to set the array of textures of the scientist
-     * @param texture the array of textures
-     * @throws IllegalArgumentException if the textures array is empty
-     */
-    public void setTextureArray(String[] texture);
-
-    //TODO: vedi commento a inizio interfaccia
-    /**
      * Method to simulate the scientist's run
      * Increment the texture index 
      * Update the position of the scientist according to the direction
      * @return the current texture of the scientist
      */
-    public String run();
+    public void nextPosition();
     
 }
--- src/main/java/it/unibo/jetpackjoyride/model/impl/ScientistImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/ScientistImpl.java
@@ -6,7 +6,6 @@ import it.unibo.jetpackjoyride.model.api.Direction;
 import it.unibo.jetpackjoyride.model.api.Scientist;
 
 /**
- * TODO: Vedi commento su texture nell'interfaccia ?
  * Standard scientist implementation
  * @author lorenzo.annibalini@studio.unibo.it
  */
@@ -15,17 +14,14 @@ public class ScientistImpl extends GameObject implements Scientist{
 
     private final Direction direction;
     private boolean life;
-    private String[] texture;
-    private int textureIndex;
 
-    public ScientistImpl(Direction direction, String[] texture, Point2d point, Vector2d velocity) {
+    public ScientistImpl(Direction direction, Point2d point, Vector2d velocity) {
         super(Type.SCIENTIST, point, velocity);
-        if(texture.length == 0 || direction == null) {
+        if(direction == null) {
             throw new IllegalArgumentException("Input can't be empty");
         }else{
             this.direction = direction;
             this.life = true;
-            this.setTextureArray(texture);
         }
     }
 
@@ -44,66 +40,7 @@ public class ScientistImpl extends GameObject implements Scientist{
         this.life = false;    
     }
 
-    @Override
-    public String[] getTextureArray() {
-        return this.texture;
-    }
-
-    @Override
-    public String getCurrentTexture() {
-        return this.texture[this.textureIndex];
-    }
-
-    @Override
-    public void setTextureArray(String[] texture) {
-        if(texture.length == 0) {
-            throw new IllegalArgumentException("Texture can't be empty");
-        }else{
-            this.texture = texture;
-        }
-    }
-
-    @Override
-    public Point2d getCurrentPos(){
-        return super.getCurrentPos();
-    }
-
-        @Override
-    public void setCurrentPos(Point2d point) {
-        if(point == null) {
-            throw new IllegalArgumentException("Point can't be null");
-        }else{ 
-            super.setPos(point);
-        } 
-    }
-
-    @Override
-    public Vector2d getVelocity() {
-        return super.getCurrentVel();
-    }
-
-    @Override
-    public void setVelocity(Vector2d velocity) {
-        if(velocity == null) {
-            throw new IllegalArgumentException("Velocity can't be null");
-        }else{ 
-            super.setVel(velocity);
-        }
-    }
-
-    @Override
-    public String run() {
-        if(this.textureIndex == this.texture.length) {
-            this.textureIndex = 0;
-        }
-        this.nextPosition();
-        return this.texture[this.textureIndex++];
-    }
-
-   private void nextPosition(){
-        //TODO: Come vogliamo gestire la velocità ? 
-        //chiamiamo run per ogni frame e usiamo la velocità per aggiornare la posizione ogni x frame ?
-        //usiamo la velocità per sommare la posizione e chiamamo run ogni x frame?
+   public void nextPosition(){
         if(direction == Direction.LEFT)
             super.setPos(super.getCurrentPos().sub(super.getCurrentVel()));
         else if(direction == Direction.RIGHT){
