9c2a05510f60397ed566da935da1300ae5d74057
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-04-03 11:38:40 +0200

    Delete all set methods and add getStartTime Changed getDuration from int to long

--- src/main/java/it/unibo/jetpackjoyride/model/api/PowerUp.java
+++ src/main/java/it/unibo/jetpackjoyride/model/api/PowerUp.java
@@ -9,16 +9,6 @@ public interface PowerUp {
     public static enum PowerUpType { SHIELD, SPEED, JETPACK, LIFE }
 
     /**
-     * active the powerup
-     */
-    public void active();
-
-    /**
-     * disable the powerup
-     */
-    public void disable();
-
-    /**
      * @return true if the powerup is active
      */
     public boolean isActive();
@@ -29,26 +19,18 @@ public interface PowerUp {
     public PowerUpType getType();
 
     /**
-     * set the cost in coin of the powerup
-     * @param cost the cost in coin of the powerup
-     * @throws IllegalArgumentException if cost is negative
-     */
-    public void setCost(int cost);
-
-    /**
      * @return the cost in coin of the powerup
      */
     public int getCost();
 
-    /**
-     * Set the duration of the powerup
-     * @param duration the duration of the powerup
-     * @throws IllegalArgumentException if duration is negative
-     */
-    public void setDuration(int duration);
 
     /**
      * @return the duration of the powerup
      */
-    public int getDuration();
+    public long getDuration();
+
+    /**
+     * @return the start time of the powerup
+     */
+    public long getStartTime();
 }
