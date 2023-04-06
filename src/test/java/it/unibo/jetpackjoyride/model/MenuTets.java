79ea5ddb3c280f9816f183dad9d918c39f5feb48
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-04-06 17:15:18 +0200

    Simple Test to run the menu

---
+++ src/test/java/it/unibo/jetpackjoyride/model/MenuTets.java
@@ -0,0 +1,10 @@
+package it.unibo.jetpackjoyride.model;
+
+import it.unibo.jetpackjoyride.model.impl.MenuSwing;
+
+public class MenuTets {
+    public static void main (String[] args) {
+        MenuSwing menu  = new MenuSwing();
+        return;
+    }
+}
