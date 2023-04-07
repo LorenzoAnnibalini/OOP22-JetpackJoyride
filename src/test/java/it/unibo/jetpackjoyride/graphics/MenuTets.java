cea688698f6345b95dba2495f5abfba34ba21499
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-04-07 15:39:54 +0200

    Moved MenuTest to graphics

---
+++ src/test/java/it/unibo/jetpackjoyride/graphics/MenuTets.java
@@ -0,0 +1,10 @@
+package it.unibo.jetpackjoyride.model;
+
+import it.unibo.jetpackjoyride.graphics.impl.MenuSwing;
+
+public class MenuTets {
+    public static void main (String[] args) {
+        MenuSwing menu  = new MenuSwing();
+        return;
+    }
+}
