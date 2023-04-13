a86c7ca64848ffaf81b3789ec2590585597c121b
Mattia Burreli <mattia.burreli@studio.unibo.it>
2023-04-13 17:59:29 +0200

    updated function calcPointPosition()

--- src/main/java/it/unibo/jetpackjoyride/model/impl/HitboxImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/HitboxImpl.java
@@ -1,11 +1,13 @@
 package it.unibo.jetpackjoyride.model.impl;
+
 import it.unibo.jetpackjoyride.common.Point2d;
 import it.unibo.jetpackjoyride.model.api.Hitbox;
 
 /**
  * This is a class to model a generic hitbox.
+ * 
  * @author mattia.burreli@studio.unibo.it
-*/
+ */
 public class HitboxImpl implements Hitbox {
 
     private Point2d upLeftPoint;
@@ -16,19 +18,19 @@ public class HitboxImpl implements Hitbox {
     /**
      * Constructor of the Hitbox.
      * Set the hitbox size and the position of the main points.
+     * 
      * @param height
      * @param width
      */
     public HitboxImpl(final int height, final int width, final Point2d posObject) {
         this.height = height;
         this.width = width;
-        this.upLeftPoint = this.calcPointPosition(posObject, "-");
-        this.downRightPoint = this.calcPointPosition(posObject, "+");
+        this.calcPointPosition(posObject);
     }
 
     @Override
     public int getWidthHitbox() {
-       return this.width;
+        return this.width;
     }
 
     @Override
@@ -38,7 +40,7 @@ public class HitboxImpl implements Hitbox {
 
     @Override
     public Point2d getPointUpLeft() {
-       return this.upLeftPoint;
+        return this.upLeftPoint;
     }
 
     @Override
@@ -48,23 +50,19 @@ public class HitboxImpl implements Hitbox {
 
     @Override
     public void updateHitbox(Point2d posObject) {
-        this.upLeftPoint = this.calcPointPosition(posObject, "-");
-        this.downRightPoint = this.calcPointPosition(posObject, "+");
+        this.calcPointPosition(posObject);
     }
-    
+
     /**
-     * method for calculating the position of a point based on the object's position and the size of its hitbox
+     * method for calculating the position of a point based on the object's position
+     * and the size of its hitbox
+     * 
      * @param posObject
      * @param sign
      * @return the position of the calculated point.
      */
-    private Point2d calcPointPosition(final Point2d posObject,String sign){
-        if(sign.equals("-")){
-            return new Point2d(posObject.x - this.width/2, posObject.y - this.height/2);
-        } else if (sign.equals("+")){
-            return new Point2d(posObject.x + this.width/2, posObject.y + this.height/2);
-        } else {
-            throw new IllegalArgumentException("Sign must be '+' or '-'");
-        }
+    private void calcPointPosition(final Point2d posObject) {
+        this.upLeftPoint = new Point2d(posObject.x - this.width / 2, posObject.y - this.height / 2);
+        this.downRightPoint = new Point2d(posObject.x + this.width / 2, posObject.y + this.height / 2);
     }
 }
