9b95ef7e7feec590c569ce10fc5c60bb42fd516d
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-04-19 18:23:34 +0200

    Started method paintComponent

--- src/main/java/it/unibo/jetpackjoyride/graphics/impl/GamePanel.java
+++ src/main/java/it/unibo/jetpackjoyride/graphics/impl/GamePanel.java
@@ -2,21 +2,26 @@ package it.unibo.jetpackjoyride.graphics.impl;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 import it.unibo.jetpackjoyride.model.impl.EntitiesGenerationImpl;
+import it.unibo.jetpackjoyride.model.impl.GameObject;
+import it.unibo.jetpackjoyride.model.impl.Rocket;
 
 /**
  * Class of the panel's game. Used to visualize map of game and sprites.
  * 
  * @author emanuele.sanchi@studio.unibo.it
  */
-public class GamePanel extends JFrame {
+public class GamePanel extends JPanel {
 
     private final EntitiesGenerationImpl entities;
     private int posImage1;
@@ -32,22 +37,27 @@ public class GamePanel extends JFrame {
 
     /**
      * Constructor of the class.
+     * 
      * @param entities model object that creates entities
      */
     public GamePanel(final EntitiesGenerationImpl e) {
         this.entities = e;
 
         try {
-            //loading background image
+            // loading background image
             backgruondImage1 = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "sfondo.jpg"));
             backgruondImage2 = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "sfondo.jpg"));
-            //loading sprite images
+            // loading sprite images and adjust sizes
             rocket = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "rocket.png"));
-            electrode = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "electrode.png"));
-            powerup = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "powerup.png"));
-            scientist = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "scientist.png"));
-            laser = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "laser.png"));
             rocket = rocket.getScaledInstance(20, 30, Image.SCALE_SMOOTH);
+            electrode = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "electrode.png"));
+            electrode = rocket.getScaledInstance(20, 30, Image.SCALE_SMOOTH);
+            powerup = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "powerup.png"));
+            powerup = rocket.getScaledInstance(20, 30, Image.SCALE_SMOOTH);
+            scientist = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "scientist.png"));
+            scientist = rocket.getScaledInstance(20, 30, Image.SCALE_SMOOTH);
+            laser = ImageIO.read(new File("resources" + GamePanel.FILESEPARATOR + "laser.png"));
+            laser = rocket.getScaledInstance(20, 30, Image.SCALE_SMOOTH);
             this.posImage1 = 0;
             this.posImage2 = backgruondImage2.getWidth();
         } catch (IOException ex) {
@@ -62,4 +72,35 @@ public class GamePanel extends JFrame {
         this.setVisible(true);
 
     }
+    @Override
+    protected void paintComponent(Graphics g) {
+        g = (Graphics2D) g;
+        super.paintComponent(g);
+        g.drawImage(backgruondImage1, this.posImage1 - slider.getPos(), 0, this);
+        g.drawImage(backgruondImage2, this.posImage2 - slider.getPos(), 0, this);
+        for (Pair<String, GameObject> el : entities.getEntities()) {
+            switch(el.getX()){
+                case "Rocket":
+                    /*g.drawImage(rocket, el.getY().getCurrentPos().x, el.getY().getCurrentPos().x, this);
+                    this.drawSprite(g, rocket, el);*/
+                    g.drawImage(rocket, el.getY().getCurrentPos().x, el.getY().getCurrentPos().x, this);
+                    break;
+                case "Electrode":
+                    break;
+                case "Scientist":
+                    break;
+                case "Powerup":
+                    break;
+                case "Laser":
+                    break;
+            }
+
+        }
+        //g.drawImage(rocket, entities.getEntities.get, 50, this);
+        
+    }
+
+    /*private void drawSprite(Graphics g, Image sprite, GameObject entity) {
+        g.drawImage(sprite, entity.getCurrentPos().x, entity.getCurrentPos().x, this);
+    }*/
 }
