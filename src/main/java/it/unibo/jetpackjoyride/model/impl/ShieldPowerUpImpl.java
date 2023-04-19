440e65f54bb3ef556730becc23638be61f6f8cf5
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-04-19 20:00:44 +0200

    ReChanged PowerUp structure

--- src/main/java/it/unibo/jetpackjoyride/model/impl/ShieldPowerUpImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/ShieldPowerUpImpl.java
@@ -1,12 +1,37 @@
 package it.unibo.jetpackjoyride.model.impl;
 
-import it.unibo.jetpackjoyride.model.api.PowerUp;
+public class ShieldPowerUpImpl extends GameObject {
 
-public class ShieldPowerUpImpl extends PowerUpImpl {
+    private boolean active;
+    private long duration;
+    private long startTime;
 
-    public ShieldPowerUpImpl(PowerUpType type, int cost, long duration) {
-        super(type, cost, duration);
-        //TODO Auto-generated constructor stub
+    public ShieldPowerUpImpl(long duration) {
+        this.duration = duration;
+        this.active = false;
+        this.startTime = 0;
     }
-    
+
+    public boolean isActive() {
+        if (this.active) {
+            if (System.currentTimeMillis() - this.startTime > this.duration) {
+                this.active = false;
+            }
+        }
+        return this.active;
+    }
+
+    public long getDuration() {
+        return this.duration;
+    }
+
+    public long getStartTime() {
+        return this.startTime;
+    }
+
+    public void setIsActive(boolean active) {
+        this.active = active;
+        this.startTime = System.currentTimeMillis();
+    }
+     
 }
--- src/main/java/it/unibo/jetpackjoyride/model/impl/SpeedUpPowerUpImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/SpeedUpPowerUpImpl.java
@@ -1,10 +1,23 @@
 package it.unibo.jetpackjoyride.model.impl;
 
-public class SpeedUpPowerUpImpl extends PowerUpImpl {
 
-    public SpeedUpPowerUpImpl(PowerUpType type, int cost, long duration) {
-        super(type, cost, duration);
-        //TODO Auto-generated constructor stub
+public class SpeedUpPowerUpImpl extends GameObject {
+
+    private boolean active;
+    private int distanceSpeedUp;
+
+    public SpeedUpPowerUpImpl(int distanceSpeedUp) {
+        this.distanceSpeedUp = distanceSpeedUp;
+        this.active = false;
     }
-    
+
+    public int active() {
+        this.active = true;
+        return this.distanceSpeedUp;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+
 }
