d451d008dd0f8ee2ab1f5cbb8e8e61d414f422bf
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-04-27 14:27:46 +0200

    Started writing the class ViewImpl

--- src/main/java/it/unibo/jetpackjoyride/graphics/impl/ViewImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/graphics/impl/ViewImpl.java
@@ -1,42 +1,68 @@
 package it.unibo.jetpackjoyride.graphics.impl;
 
+import javax.swing.JFrame;
+
 import it.unibo.jetpackjoyride.graphics.api.View;
+import it.unibo.jetpackjoyride.model.api.EntitiesGenerator;
+import it.unibo.jetpackjoyride.model.impl.EntitiesGeneratorImpl;
+
+
+/*
+ * Implements the View interface of the game.
+ * 
+ * @author lorenzo.annibalini@studio.unibo.it
+ */
 
 public class ViewImpl implements View {
 
-    public static void main(String args[]) {
-        new GamePanel();
-    }
+    //TODO: Add Shop, Game and Statistics panels
+    
+    private final JFrame frame;
+    private final GamePanel game;
+    private final MenuPanel menuPanel;
+    //private final EndGamePanel endGame;
+    //private final ShopPanel shop;
+    //private final StatisticsPanel statistics;
 
+    public ViewImpl(final EntitiesGeneratorImpl e) {
+        this.frame = new JFrame("Jetpack Joyride");
+        this.game = new GamePanel(e);
+        this.menuPanel = new MenuPanel();
+        //this.shop = new ShopPanel();
+        //this.statistics = new StatisticsPanel();
+       
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.frame.setSize(this.menuPanel.getPreferredSize());
+        this.frame.setMinimumSize(this.menuPanel.getPreferredSize());
+        this.frame.pack();
+        this.frame.getContentPane().add(this.menuPanel);
+        this.frame.setVisible(true);
+    }
 
     @Override
     public void renderGame() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'renderGame'");
+        this.game.repaint();
     }
 
     @Override
     public void renderMenu() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'renderMenu'");
+        this.menuPanel.repaint();
     }
 
     @Override
     public void renderShop() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'renderShop'");
+        this.shop.repaint();
     }
 
     @Override
     public void renderEndGame() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'renderEndGame'");
+        //TODO: cosa devo fare qui? Chiamo statistics ?
+        this.renderStatistics();
     }
 
     @Override
     public void renderStatistics() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'renderStatistics'");
+        this.statistics.repaint();
     }
     
 }
