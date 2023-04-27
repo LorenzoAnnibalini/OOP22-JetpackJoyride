7a5f82844fb9f8865e4529bf7949b809d27e2e94
Emanuele Sanchi <emanuele.sanchi@studio.unibo.it>
2023-04-27 14:46:20 +0200

    Add fontstyle and dimension

--- src/main/java/it/unibo/jetpackjoyride/graphics/impl/StatisticsPanel.java
+++ src/main/java/it/unibo/jetpackjoyride/graphics/impl/StatisticsPanel.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +13,6 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
 
 /**
  * Class to visualize the statistics of the game
@@ -23,7 +23,10 @@ public class StatisticsPanel extends JPanel {
     private Map<String, Integer> statistics;
     private BufferedImage backgruondImage;
     private static final String FILESEPARATOR = File.separator;
-    JTextArea statsVisualizer;
+    private static final String FONTNAME = "Verdana";
+    private static final int FONTSTYLE = 0;
+    private static final int FONTSIZE = 15;
+
 
     /**
      * Constructor of the class.
@@ -33,14 +36,11 @@ public class StatisticsPanel extends JPanel {
      */
     public StatisticsPanel(Map<String, Integer> statistics) {
         this.statistics = new HashMap<>(statistics);
-        try{
+        try {
             this.backgruondImage = ImageIO.read(new File("resources" + StatisticsPanel.FILESEPARATOR + "sfondo.jpg"));
-        } catch (IOException ex){
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
-        statsVisualizer = new JTextArea();
-
-        this.add(statsVisualizer);
 
         this.setPreferredSize(new Dimension(backgruondImage.getWidth(), backgruondImage.getHeight()));
         this.setSize(this.getPreferredSize());
@@ -48,14 +48,14 @@ public class StatisticsPanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
+        this.removeAll();
         g = (Graphics2D) g;
         g.drawImage(backgruondImage, 0, 0, this);
-        statsVisualizer.removeAll();
         for (String statName : this.statistics.keySet()) {
             int value = statistics.get(statName);
             String text = statName + "\t\t\t\t " + value;
-            this.add(new JLabel(text));
-            
+            JLabel label = new JLabel(text);
+            label.setFont(new Font(StatisticsPanel.FONTNAME, StatisticsPanel.FONTSTYLE, StatisticsPanel.FONTSIZE));
         }
     }
 }
