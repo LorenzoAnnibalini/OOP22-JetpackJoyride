4eca83e31a3e1524fb49b8c22da4c5418a052bec
Mattia Burreli <mattia.burreli@studio.unibo.it>
2023-04-27 19:57:31 +0200

    added functions for GameEngineImpl

--- src/main/java/it/unibo/jetpackjoyride/core/api/GameEngine.java
+++ src/main/java/it/unibo/jetpackjoyride/core/api/GameEngine.java
@@ -1,5 +1,7 @@
 package it.unibo.jetpackjoyride.core.api;
 
+import it.unibo.jetpackjoyride.input.api.Input;
+
 /**
  * Interface for the game engine.
  * It is the core of the game, it manages the game loop and the game state.
@@ -12,4 +14,9 @@ public interface GameEngine {
      * Start the game engine.
      */
     public void worldGameStateStart();
+
+    /**
+     * Notify an intup event to the game engine, it will be processed in the next game loop.
+     */
+    public void notifyInput(final Input input);
 }
--- src/main/java/it/unibo/jetpackjoyride/core/impl/GameEngineImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/core/impl/GameEngineImpl.java
@@ -15,6 +15,13 @@ public class GameEngineImpl implements GameEngine {
     private long framePeriod = 20;
     private WorldGameState worldGameState;
 
+    private enum state {
+        MENU,
+        GAME,
+        GAMEOVER
+    }
+
+
     public GameEngineImpl() {
         this.inputHandler = new InputQueueImpl();
         this.view = new ViewImpl();
@@ -24,9 +31,46 @@ public class GameEngineImpl implements GameEngine {
     public void worldGameStateStart() {
         this.worldGameState = new WorldGameStateImpl();
     }
- 
-    
 
+ 
+    private void loopState(){
+        long previousCycleStartTime = System.currentTimeMillis();
+        while (true){
+            long currentCycleStartTime = System.currentTimeMillis();
+            long elapsedTime = currentCycleStartTime - previousCycleStartTime;
+            this.processInput();
+            this.updateWorldGameState();
+            this.renderView();
+            this.waitNextFrame(currentCycleStartTime);
+            previousCycleStartTime = currentCycleStartTime;
+        }
+    }
+
+    private void processInput(){
+
+    }
     
+    private void updateWorldGameState(){
+
+    }
     
+    private void renderView(){
+        view.render();
+    }
+
+    private void waitNextFrame(final long cycleStartTime){
+        long dt = System.currentTimeMillis() - cycleStartTime;
+		if (dt < this.framePeriod){
+			try {
+				Thread.sleep(this.framePeriod - dt);
+			} catch (Exception ex){}
+		}
+    }
+
+    @Override
+    public void notifyInput(final Input input) {
+        this.inputHandler.addInput(input);
+    }
+
+
 }
