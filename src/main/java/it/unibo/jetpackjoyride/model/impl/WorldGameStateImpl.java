109ee425a0c3a071faaabf3fef93c7e4347421d9
Mattia Burreli <mattia.burreli@studio.unibo.it>
2023-04-29 19:51:06 +0200

    minor changes

--- src/main/java/it/unibo/jetpackjoyride/model/impl/WorldGameStateImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/WorldGameStateImpl.java
@@ -2,9 +2,7 @@ package it.unibo.jetpackjoyride.model.impl;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import java.util.Set;
 
-import it.unibo.jetpackjoyride.common.Pair;
 import it.unibo.jetpackjoyride.common.Point2d;
 import it.unibo.jetpackjoyride.common.Vector2d;
 import it.unibo.jetpackjoyride.model.api.EntitiesGenerator;
@@ -15,8 +13,6 @@ import it.unibo.jetpackjoyride.model.api.WorldGameState;
 public class WorldGameStateImpl implements WorldGameState{
     
     private Statistics runStatistics;
-    private Set<Pair<String, GameObject>> entities;
-    private Set<Pair<String, GameObject>> entitiesQueue;
     private EntitiesGenerator entitiesGenerator;
     private Player player;
     private List<Money> money;
@@ -27,13 +23,10 @@ public class WorldGameStateImpl implements WorldGameState{
         this.runStatistics.addStatistic("money", 0);
         this.runStatistics.addStatistic("score", 0);
         this.player=new PlayerImpl(new Point2d(50,350), new Vector2d(50, 350),new HitboxImpl(15, 10, new Point2d(50,350)));
-        this.newEntitiesGeneration();
-        this.entitiesQueue=this.entitiesGenerator.getEntities();
     }
 
 
     private void newEntitiesGeneration(){
-        if (entitiesQueue.isEmpty()){
             try {
                 this.entitiesGenerator.generateEntity();
             } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
@@ -41,8 +34,20 @@ public class WorldGameStateImpl implements WorldGameState{
                 e.printStackTrace();
             }
         }
+
+
+    public void updateState(){
+
+        
+
     }
 
 
 
+
+
+
+
+
 }
+
