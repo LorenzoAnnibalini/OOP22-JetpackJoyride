ee7b78e51ba1d0d0053b4656e65331aa300387b2
Mattia Burreli <mattia.burreli@studio.unibo.it>
2023-05-04 17:12:08 +0200

    added functions for check the collision between two hitbox or between an hitbox and a point

--- src/main/java/it/unibo/jetpackjoyride/model/api/Hitbox.java
+++ src/main/java/it/unibo/jetpackjoyride/model/api/Hitbox.java
@@ -4,30 +4,35 @@ import it.unibo.jetpackjoyride.common.Point2d;
 
 /**
  * Interface for the hitbox.
+ * 
  * @author mattia.burreli@studio.unibo.it
-*/
+ */
 public interface Hitbox {
-    
+
     /**
      * Method to get the width of the hitbox.
+     * 
      * @return the position of the hitbox.
      */
     public int getWidthHitbox();
 
     /**
      * Method to get the height of the hitbox.
+     * 
      * @return the position of the hitbox.
      */
     public int getHeigthHitbox();
 
     /**
      * Method to get the position of the up-left point of hitbox.
+     * 
      * @return the position of the up-left point.
      */
     public Point2d getPointUpLeft();
 
     /**
      * Method to get the position of the down-right point of hitbox.
+     * 
      * @return the position of the down-right point.
      */
     public Point2d getPointDownRight();
@@ -36,5 +41,13 @@ public interface Hitbox {
      * Method to update the position of the hitbox and main points.
      */
     public void updateHitbox(final Point2d posObject);
+
+    /**
+     * Method to check if two hitboxes are in collision.
+     * 
+     * @param hitbox
+     * @return true if the hitboxes are in collision, false otherwise.
+     */
+    public boolean checkCollision(final Hitbox hitbox);
 
 }
