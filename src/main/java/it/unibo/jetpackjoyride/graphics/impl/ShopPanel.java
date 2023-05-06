6017c9e891bb69d39d2ddd478a3bd93e67fa4e27
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-05-06 15:48:42 +0200

    Test version of the ShopPanel for the skin

--- src/main/java/it/unibo/jetpackjoyride/graphics/impl/ShopPanel.java
+++ src/main/java/it/unibo/jetpackjoyride/graphics/impl/ShopPanel.java
@@ -14,10 +14,13 @@ import javax.swing.JPanel;
 
 import it.unibo.jetpackjoyride.model.api.Gadget;
 import it.unibo.jetpackjoyride.model.impl.GadgetImpl;
+import it.unibo.jetpackjoyride.model.api.SkinInfo;
+import it.unibo.jetpackjoyride.model.impl.SkinInfoImpl;
 import it.unibo.jetpackjoyride.input.api.InputQueue;
 import it.unibo.jetpackjoyride.input.api.Input.typeInput;
 import it.unibo.jetpackjoyride.input.impl.InputImpl;
 import it.unibo.jetpackjoyride.core.api.GadgetInfoPositions;
+import it.unibo.jetpackjoyride.core.api.SkinInfoPositions;
 
 /**
  * Class that represents the panel of the shop.
@@ -29,7 +32,9 @@ public class ShopPanel extends JPanel{
     private final JButton menu;
     private final InputQueue inputQueue;
     private final Gadget gadget;
-    private HashMap<String, ArrayList<JButton>> buttonMap;
+    private final SkinInfo skinInfo;
+    private HashMap<String, ArrayList<JButton>> buttonMapGadget;
+    private HashMap<String, ArrayList<JButton>> buttonMapSkin;
 
     /**
      * Constructor for the ShopPanel.
@@ -38,7 +43,8 @@ public class ShopPanel extends JPanel{
     public ShopPanel(final InputQueue inputQueue) {
         super();
         this.inputQueue = inputQueue;
-        this.buttonMap = new HashMap<>();
+        this.buttonMapGadget = new HashMap<>();
+        this.buttonMapSkin = new HashMap<>();
         this.setLayout(new BorderLayout());
         JPanel boxPanel = new JPanel();
         boxPanel.setLayout(new BoxLayout(boxPanel, BoxLayout.Y_AXIS));
@@ -58,18 +64,33 @@ public class ShopPanel extends JPanel{
             String price = gadget.getValue(name).get(GadgetInfoPositions.PRICE.ordinal());
             String description = gadget.getValue(name).get(GadgetInfoPositions.DESCRIPTION.ordinal());
 
-            JPanel FlowPanel = new JPanel(new FlowLayout());
-            FlowPanel.add(new JLabel(name));
-            FlowPanel.add(new JLabel(price));
-            FlowPanel.add(new JLabel(description));
+            JPanel flowPanel = new JPanel(new FlowLayout());
+            flowPanel.add(new JLabel(name));
+            flowPanel.add(new JLabel(price));
+            flowPanel.add(new JLabel(description));
             JButton enableButton = createGadgetButton(Boolean.parseBoolean(state) == true ? "Disable" : "Enable", true, name);
             JButton purchasedButton = createGadgetButton("Purchased", !Boolean.parseBoolean(purchased), name);
-            buttonMap.put(name, new ArrayList<>(List.of(enableButton, purchasedButton)));
-            FlowPanel.add(purchasedButton);
-            FlowPanel.add(enableButton);
-            boxPanel.add(FlowPanel);
+            buttonMapGadget.put(name, new ArrayList<>(List.of(enableButton, purchasedButton)));
+            flowPanel.add(purchasedButton);
+            flowPanel.add(enableButton);
+            boxPanel.add(flowPanel);
         }
+        
         boxPanel.add(new JLabel("Skin"));
+        skinInfo = new SkinInfoImpl();
+        for (String name : skinInfo.getAll().keySet()) {
+            String state = skinInfo.getValue(name).get(SkinInfoPositions.STATE.ordinal());
+            String purchased = skinInfo.getValue(name).get(SkinInfoPositions.PURCHASED.ordinal());
+
+            JPanel flowPanel = new JPanel(new FlowLayout());
+            flowPanel.add(new JLabel(name));
+            JButton enableButton = createSkinButton("Enable", !Boolean.parseBoolean(state), name);
+            JButton purchasedButton = createSkinButton("Purchased", !Boolean.parseBoolean(purchased), name);
+            buttonMapSkin.put(name, new ArrayList<>(List.of(enableButton, purchasedButton)));
+            flowPanel.add(purchasedButton);
+            flowPanel.add(enableButton);
+            boxPanel.add(flowPanel);
+        }
     }
 
     /**
@@ -100,17 +121,47 @@ public class ShopPanel extends JPanel{
         return button;
     }
 
+    private JButton createSkinButton(String text, boolean enabled, String name){
+        JButton button = new JButton(text);
+        button.setEnabled(enabled);
+        button.addActionListener(e -> {
+            switch (button.getText()) {
+            case "Enable":
+                this.inputQueue.addInput(new InputImpl(typeInput.SELECT_SKIN, name));
+                skinInfo.getAll().values().forEach(list -> list.set(SkinInfoPositions.STATE.ordinal(), "false"));
+                skinInfo.setValue(name, "true", skinInfo.getValue(name).get(SkinInfoPositions.PURCHASED.ordinal()));
+                this.update();
+                break;
+            case "Purchased":
+                this.inputQueue.addInput(new InputImpl(typeInput.BUY_SKIN, name));
+                skinInfo.setValue(name, skinInfo.getValue(name).get(SkinInfoPositions.STATE.ordinal()), "true");
+                this.update();
+                break;
+            default:
+                break;
+            }
+        });
+        return button;
+    }
+
     /**
      * This method update the panel recalulating the button state
      * by reading new states from the gadget values.
      */
     public void update(){
-        for (String name : buttonMap.keySet()) {
-            ArrayList<JButton> buttonList = buttonMap.get(name);
+        for (String name : buttonMapGadget.keySet()) {
+            ArrayList<JButton> buttonList = buttonMapGadget.get(name);
             String purchased = gadget.getValue(name).get(GadgetInfoPositions.PURCHASED.ordinal());
             String state = (Boolean.parseBoolean(gadget.getValue(name).get(GadgetInfoPositions.STATE.ordinal())) == true ? "Disable" : "Enable");
             buttonList.get(GadgetInfoPositions.PURCHASED.ordinal()).setEnabled(!Boolean.parseBoolean(purchased));
             buttonList.get(GadgetInfoPositions.STATE.ordinal()).setText(state); 
+        }
+        for (String name : buttonMapSkin.keySet()) {
+            ArrayList<JButton> buttonList = buttonMapSkin.get(name);
+            String purchased = skinInfo.getValue(name).get(SkinInfoPositions.PURCHASED.ordinal());
+            String state = skinInfo.getValue(name).get(SkinInfoPositions.STATE.ordinal());
+            buttonList.get(GadgetInfoPositions.PURCHASED.ordinal()).setEnabled(!Boolean.parseBoolean(purchased));
+            buttonList.get(GadgetInfoPositions.STATE.ordinal()).setEnabled(!Boolean.parseBoolean(state));
         }
     }
 
