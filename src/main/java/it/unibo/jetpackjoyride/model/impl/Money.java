3c8f082761c2b1100ccb33fc5af17c62dd8b9a2c
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-05-09 12:44:00 +0200

    Added static method to Money to create an ArrayList<money> from an ArrayList<String>

--- src/main/java/it/unibo/jetpackjoyride/model/impl/Money.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/Money.java
@@ -1,5 +1,7 @@
 package it.unibo.jetpackjoyride.model.impl;
 
+import java.util.ArrayList;
+
 import it.unibo.jetpackjoyride.common.Point2d;
 import it.unibo.jetpackjoyride.common.Vector2d;
 import it.unibo.jetpackjoyride.model.api.Hitbox;
@@ -13,5 +15,20 @@ public class Money extends GameObject{
     
     public Money(final Point2d pos, final Vector2d vel,final Hitbox hitbox){
         super(pos, vel, hitbox);
+    }
+
+    public static ArrayList<Money> getMoneyFromStringList(final ArrayList<String> moneyString){
+        final int X = 0;
+        final int Y = 1;
+        ArrayList<Money> moneyList = new ArrayList<>();
+        moneyString.stream().map(e -> e.split(",")).forEach(e -> {
+            int x =  Integer.parseInt(e[X]);
+            int y =  Integer.parseInt(e[Y]);
+            Point2d pos = new Point2d(x, y);
+            Vector2d vec = new Vector2d(0, y);
+            Hitbox hitbox = new HitboxImpl(5, 5, pos);
+            moneyList.add(new Money(pos, vec, hitbox));
+        });
+        return moneyList;
     }
 }
