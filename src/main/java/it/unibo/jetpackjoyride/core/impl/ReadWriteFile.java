c64fd0326d91d0beaf0468235a318e9d60bd1d9f
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-05-15 14:07:14 +0200

    ReadMap : OK

--- src/main/java/it/unibo/jetpackjoyride/core/impl/ReadWriteFile.java
+++ src/main/java/it/unibo/jetpackjoyride/core/impl/ReadWriteFile.java
@@ -1,7 +1,14 @@
 package it.unibo.jetpackjoyride.core.impl;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 /**
  * A simple class to read and write different tipe of files.
@@ -13,8 +20,12 @@ import java.util.Map;
 public class ReadWriteFile<T, G> {
 
     private String path;
-    private Map<T, G> map;
-    private ArrayList<T> list;
+
+   public ReadWriteFile(final String path) {
+        System.out.println(path);
+        this.path = getClass().getClassLoader().getResource(path).getPath();
+        System.out.println(this.path);
+    }
 
     /**
      * @param path the path of the file to read or write
@@ -32,26 +43,52 @@ public class ReadWriteFile<T, G> {
 
     /**
      * @param map the map to write in the file
+     * @throws IOException
      */
-    public void writeMap(final Map<T,G> map){
-        //TODO
+    public void writeMap(final Map<T,G> map) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new FileWriter(this.path));
+        for (T key : map.keySet()) {
+            writer.write(key + ";" + map.get(key) + "\n");
+        }
+        writer.close();
+        
     }
 
     /**
      * @return the file read as a map
+     * @throws FileNotFoundException
      */
-    public <T, G> Map<T, G> readMap(){
-        //TODO
-        return null;
+    public <T, G> Map<T, G> readMap() throws FileNotFoundException{
+        Map<T, G> mappaTmp = new HashMap<T, G>();
+        Scanner sc = new Scanner(new File(this.path));
+        sc.useDelimiter(";"); 
+            while (sc.hasNextLine()) { 
+                    T key = (T)sc.nextLine();
+                    G value = (G)sc.nextLine(); 
+                    mappaTmp.put(key, value);
+                    System.out.println(mappaTmp.get(key));
+                    System.out.println("Reading Game Settings : " + key + " " + value);
+            }
+        sc.close();  
+        return mappaTmp;
     }
 
-    public void writeArrayList(final ArrayList<T> list){
-        //TODO
+    public void writeArrayList(final ArrayList<T> list) throws IOException{
+            BufferedWriter writer = new BufferedWriter(new FileWriter(this.path));
+            for (T element : list) {
+                writer.write(element + "\n");
+            }
+            writer.close();
     }
 
-    public <T> ArrayList<T> readArrayList(){
-        //TODO
-        return null;
+    public <T> ArrayList<T> readArrayList() throws FileNotFoundException{
+        Scanner file = new Scanner(new File(this.path));
+        ArrayList<T> list = new ArrayList<T>();
+        while (file.hasNextLine()) {
+            String line = file.nextLine();
+            list.add((T) line);
+        }
+        return list;
     }
 
 
