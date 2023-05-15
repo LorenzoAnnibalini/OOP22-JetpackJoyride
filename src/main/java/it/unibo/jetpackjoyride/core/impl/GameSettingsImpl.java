a4bdfb792dbfa80e76d35ac3139451cc1d068e58
Lorenzo Annibalini <lorenzo.annibalini@studio.unibo.it>
2023-05-15 14:26:17 +0200

    re-write GameSettingsImpl to use ReadWriteFile

--- src/main/java/it/unibo/jetpackjoyride/core/impl/GameSettingsImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/core/impl/GameSettingsImpl.java
@@ -3,11 +3,8 @@ package it.unibo.jetpackjoyride.core.impl;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
-
 
 import it.unibo.jetpackjoyride.core.api.GameSettings;
 
@@ -26,24 +23,14 @@ public class GameSettingsImpl implements GameSettings {
      * Read the file and put the values in the map
      * @throws FileNotFoundException if the file is not found
      */
-    public GameSettingsImpl() throws FileNotFoundException {
-
-
-        //TODO : USARE CLASSE GENERALE READ WRITE FILES
-        
-        Scanner sc = new Scanner(new File(getClass().getProtectionDomain().getCodeSource().getLocation()+"game_settings.csv"));  
-        sc.useDelimiter(";"); 
-        try{ 
-            while (sc.hasNext()) { 
-                    String key = sc.next();
-                    String value = sc.next();  
-                    settings.put(key, value);
-                    System.out.println("Reading Game Settings : " + key + " " + value);
-            }
+    public GameSettingsImpl() {
+        try{
+            ReadWriteFile<String,String> reader = new ReadWriteFile<String,String>("gamesettings.csv");
+            settings.putAll(reader.readMap());
         }catch(Exception e){
-            System.out.println("Reading Game Settings : Error");
+            System.out.println("Reading Game Settings : ERROR");
+            System.out.println(e.toString());
         }
-        sc.close();  
         if(settings.isEmpty()){
             System.out.println("Reading Game Settings : Empty File -> Default Settings");
             settings.put("audio", "Audio OFF");
@@ -67,22 +54,13 @@ public class GameSettingsImpl implements GameSettings {
     }
 
     @Override
-    public void writeSettings() throws IOException{
-        FileWriter csvWriter;
+    public void writeSettings(){
         try{
-
-            //TODO : USARE CLASSE GENERALE READ WRITE FILES
-
-            csvWriter = new FileWriter(getClass().getProtectionDomain().getCodeSource().getLocation()+"game_settings.csv");
-            for (Map.Entry<String, String> entry : settings.entrySet()) {
-                csvWriter.append(entry.getKey());
-                csvWriter.append(";");
-                csvWriter.append(entry.getValue());
-                csvWriter.append(";");
-            }
-            csvWriter.close();
-        }catch(IOException e){
+            ReadWriteFile<String,String> reader = new ReadWriteFile<String,String>("gamesettings.csv");
+            reader.writeMap(settings);
+        }catch(Exception e){
             System.out.println("Writing Game Settings : Error");
+            System.out.println(e.toString());
         }
     }
 
