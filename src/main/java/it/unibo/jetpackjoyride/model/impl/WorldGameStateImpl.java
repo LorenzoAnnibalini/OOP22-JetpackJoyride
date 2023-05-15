5df9dd17249a892b1a98fe097e405ae543af33c6
Mattia Burreli <mattia.burreli@studio.unibo.it>
2023-05-15 10:28:00 +0200

    minor changes in inizializeWorldGameState, created function randomDecider for generate a random number between 0 and 2 for newEntities function, minor changes

--- src/main/java/it/unibo/jetpackjoyride/model/impl/WorldGameStateImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/WorldGameStateImpl.java
@@ -31,6 +31,10 @@ public class WorldGameStateImpl implements WorldGameState {
     private static final int START_NUMBER_DECIDER = 0;
     private static final int TIME_TO_WAIT = 5000;
     private static final int SPEED_POWERUP_DISTANCE = 1000;
+    private static final int GENERAL_PROBABILITY = 10;
+    private static final int MONEY_PROBABILITY = 3;
+    private static final int LASER_PROBABILITY = 2;
+    private static final int ENTITY_PROBABILITY = 5;
     private Statistics runStatistics;
     private EntitiesGenerator entitiesGenerator;
     private PlayerImpl player;
@@ -40,7 +44,7 @@ public class WorldGameStateImpl implements WorldGameState {
     private MoneyPatternLoader moneyPatternLoader;
     private SavesImpl saves;
     private Random random;
-    private int deciderEntitiesGenerator; // 0-2 = entities, 3 = money, 4 = laser
+    private int deciderEntitiesGenerator; // 0 = entities, 1 = money, 2 = laser
     private InputQueue inputHandler;
     private Statistics generalStatistics;
 
@@ -48,7 +52,14 @@ public class WorldGameStateImpl implements WorldGameState {
         this.inizializeWorldGameState();
         this.inputHandler = inputHandler;
         this.generalStatistics = new StatisticsImpl();
-        // this.generalStatistics.setAll(this.saves.downloadSaves());
+        this.saves=new SavesImpl();
+        this.moneyPatternLoader = new MoneyPatternLoaderImpl();
+        this.random = new Random();
+        try {
+            this.generalStatistics.setAll(this.saves.downloadSaves());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -71,29 +82,33 @@ public class WorldGameStateImpl implements WorldGameState {
     }
 
     /**
-     * Create new entities in the world.
+     * Create new entities in the world. If the number of entities is less than a
+     * certain number and the time passed is greater than a certain time and
+     * the decider is 0, create new entities. Else if the decider is 1, create new
+     * money. Else if the decider is 2 and there are no entities, create new
+     * lasers. If there are no scientists in the world, create new scientists.
      */
     private void newEntities() {
         long currentCycleStartTime = System.currentTimeMillis();
         if (this.entities.size() - this.entities.stream()
                 .filter(entity -> entity.getX().matches("Scientist") || entity.getX().matches("Nothing"))
                 .count() <= ENTITIES_NUMBER && currentCycleStartTime - this.previousCycleStartTime >= TIME_TO_WAIT
-                && this.deciderEntitiesGenerator <= 2) {
+                && this.deciderEntitiesGenerator == 0) {
             this.entitiesGenerator.generateEntity(this.entities, random.nextInt(2) + 2);
             this.entities = this.entitiesGenerator.getEntities();
             this.previousCycleStartTime = currentCycleStartTime;
-            this.deciderEntitiesGenerator = random.nextInt(5);
+            this.deciderEntitiesGenerator = this.randomDecider();
         } else if (currentCycleStartTime - this.previousCycleStartTime >= TIME_TO_WAIT
-                && this.deciderEntitiesGenerator == 3) {
+                && this.deciderEntitiesGenerator == 1) {
             try {
                 this.money = moneyPatternLoader.getMoneyPattern();
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
             this.previousCycleStartTime = currentCycleStartTime;
-            this.deciderEntitiesGenerator = random.nextInt(5);
+            this.deciderEntitiesGenerator = this.randomDecider();
         } else if (currentCycleStartTime - this.previousCycleStartTime >= TIME_TO_WAIT
-                && this.deciderEntitiesGenerator == 4 && this.entities.size() == 0) {
+                && this.deciderEntitiesGenerator == 2 && this.entities.size() == 0) {
             this.entitiesGenerator.generateLaser(this.entities, random.nextInt(4));
             this.entities = this.entitiesGenerator.getEntities();
             this.previousCycleStartTime = currentCycleStartTime;
@@ -210,8 +225,6 @@ public class WorldGameStateImpl implements WorldGameState {
         this.entitiesGenerator = new EntitiesGeneratorImpl();
         this.entities = new HashSet<>();
         this.money = new ArrayList<>();
-        this.moneyPatternLoader = new MoneyPatternLoaderImpl();
-        this.random = new Random();
         this.runStatistics.addStatistic("Money", 0);
         this.runStatistics.addStatistic("Meters", 0);
         this.player = new PlayerImpl(new Point2d(50, 350), new Vector2d(50, 350),
@@ -238,7 +251,7 @@ public class WorldGameStateImpl implements WorldGameState {
      */
     private void notifyEndGame() {
         this.inputHandler.addInput(new InputImpl(Input.typeInput.END_GAME, "endGame"));
-        // this.generalStatistics.addAll(this.runStatistics);
+        this.generalStatistics.updateGeneralStats(this.runStatistics);
         try {
             this.saves.uploadSaves(this.generalStatistics.getAll());
         } catch (IOException e) {
@@ -256,7 +269,7 @@ public class WorldGameStateImpl implements WorldGameState {
         if (this.deciderEntitiesGenerator == 4
                 && this.entities.stream().filter(entity -> entity.getX().equals("Laser")).count() != 0) {
             while (entityIterator.hasNext()) {
-                Pair<String,GameObject> laserRay = entityIterator.next();
+                Pair<String, GameObject> laserRay = entityIterator.next();
                 LaserRay laserRayObj = (LaserRay) laserRay.getY();
                 laserRayObj.checkState(1);
                 if (laserRayObj.isEnd()) {
@@ -267,6 +280,25 @@ public class WorldGameStateImpl implements WorldGameState {
             }
         }
 
+    }
+
+    /**
+     * Method to generate a random number between 0 and 3. This number is used to
+     * decide what type of object has to be generated.
+     * 
+     * @return the number generated.
+     */
+    private int randomDecider() {
+        int valueRandom = random.nextInt(GENERAL_PROBABILITY);
+        if (valueRandom < ENTITY_PROBABILITY) {
+            return 0;
+        } else if (valueRandom < ENTITY_PROBABILITY + MONEY_PROBABILITY) {
+            return 1;
+        } else if (valueRandom < ENTITY_PROBABILITY + MONEY_PROBABILITY + LASER_PROBABILITY) {
+            return 2;
+        } else {
+            return 3;
+        }
     }
 
     @Override
