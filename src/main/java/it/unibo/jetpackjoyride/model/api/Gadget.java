b1eb11341daff13cd9de1c5cc64d6f1f5f2edf58
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-05-23 10:17:45 +0200

    refactoring Gadget interface

--- src/main/java/it/unibo/jetpackjoyride/model/api/Gadget.java
+++ src/main/java/it/unibo/jetpackjoyride/model/api/Gadget.java
@@ -5,13 +5,15 @@ import java.util.Map;
 
 /**
  * Interface for Gadget information.
+ * 
  * @author lorenzo.bacchini4@studio.unibo.it
  */
 
 public interface Gadget {
-    
+
     /**
      * Get map of Gadget information.
+     * 
      * @return map of Gadget information
      */
     Map<String, List<String>> getAll();
@@ -27,19 +29,19 @@ public interface Gadget {
     /**
      * Setter for a Gadget.
      * 
-     * @param name  the name of the Gadget
-     * @param state the state of the Gadget (active or not)
-     * @param purchased if the gadget was purchased true otherwise false
-     * @param price the price of the gadget
+     * @param name        the name of the Gadget
+     * @param state       the state of the Gadget (active or not)
+     * @param purchased   if the gadget was purchased true otherwise false
+     * @param price       the price of the gadget
      * @param description the description of the gadget
      */
-    void setValue(String name, String state, String purchased, 
-        String price, String description);
+    void setValue(String name, String state, String purchased,
+            String price, String description);
 
-    
     /**
      * Setter for a Gadget.
-     * @param name the name of the gadget
+     * 
+     * @param name  the name of the gadget
      * @param value the List of strings that contains the information of the gadget
      */
     void setValue(String name, List<String> value);
