b6bb242fc476e2652d82c18a339c108b8bbfba86
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-05-23 10:08:59 +0200

    refactoring ObstacleImpl class

--- src/main/java/it/unibo/jetpackjoyride/model/impl/ObstacleImpl.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/ObstacleImpl.java
@@ -7,37 +7,39 @@ import it.unibo.jetpackjoyride.model.api.Obstacle;
 
 /**
  * This is a class to model a generic obstacle.
+ * 
  * @author lorenzo.bacchini4@studio.unibo.it
  */
 
-public abstract class ObstacleImpl extends GameObject implements Obstacle{
+public abstract class ObstacleImpl extends GameObject implements Obstacle {
 
     private boolean active;
 
     /**
-     * Constructor of the Obstacle 
+     * Constructor of the Obstacle
      * call back the constructor of the superclass
      * and set the obstacle state to active.
+     * 
      * @param type
      * @param pos
      * @param vel
      */
-    public ObstacleImpl(Point2d pos, Vector2d vel, Hitbox hitbox) {
+    public ObstacleImpl(final Point2d pos, final Vector2d vel, final Hitbox hitbox) {
         super(pos, vel, hitbox);
         this.setActiveOn();
     }
-    
-    public void setActiveOn(){
+
+    public void setActiveOn() {
         this.active = true;
         this.getHitbox().setHitboxActive();
     }
 
-    public void setActiveOff(){
+    public void setActiveOff() {
         this.active = false;
         this.getHitbox().setHitboxDisable();
     }
 
-    public boolean isActive(){
+    public boolean isActive() {
         return this.active;
     }
 }
