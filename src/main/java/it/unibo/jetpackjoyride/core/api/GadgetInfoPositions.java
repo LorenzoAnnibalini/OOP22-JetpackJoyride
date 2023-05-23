6824253b586f985949aee743919eb6b6c446dc5c
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-05-23 10:43:27 +0200

    refactoring GadgetInfoPositions enum

--- src/main/java/it/unibo/jetpackjoyride/core/api/GadgetInfoPositions.java
+++ src/main/java/it/unibo/jetpackjoyride/core/api/GadgetInfoPositions.java
@@ -2,7 +2,8 @@ package it.unibo.jetpackjoyride.core.api;
 
 /**
  * Enum to get the position of the gadget information.
- *@author lorenzo.bacchini4@studio.unibo.it
+ * 
+ * @author lorenzo.bacchini4@studio.unibo.it
  */
 public enum GadgetInfoPositions {
     STATE,
