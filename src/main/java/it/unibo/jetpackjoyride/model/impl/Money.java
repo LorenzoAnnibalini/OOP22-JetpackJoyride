4767a767a6c0c3290d056b8d0de8f1421a95eb0d
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2023-06-06 23:33:31 +0200

    removed unused method

--- src/main/java/it/unibo/jetpackjoyride/model/impl/Money.java
+++ src/main/java/it/unibo/jetpackjoyride/model/impl/Money.java
@@ -1,8 +1,5 @@
 package it.unibo.jetpackjoyride.model.impl;
 
-import java.util.List;
-import java.util.ArrayList;
-
 import it.unibo.jetpackjoyride.common.Point2d;
 import it.unibo.jetpackjoyride.common.Vector2d;
 import it.unibo.jetpackjoyride.model.api.Hitbox;
@@ -15,9 +12,6 @@ import it.unibo.jetpackjoyride.model.api.Hitbox;
 
 public class Money extends GameObjectImpl {
 
-    private static final int X = 0;
-    private static final int Y = 1;
-
     /**
      * Cosntructor of the class Money.
      * @param pos position
@@ -28,22 +22,4 @@ public class Money extends GameObjectImpl {
         super(pos, vel, hitbox);
     }
 
-    /**
-     * Static method to create a list of money from a list of string.
-     * 
-     * @param moneyString the list of string to convert
-     * @return moneyList, the list of money
-     */
-    public static List<Money> getMoneyFromStringList(final List<String> moneyString) {
-        final ArrayList<Money> moneyList = new ArrayList<>();
-        moneyString.stream().map(e -> e.split(",")).forEach(e -> {
-            final int x = Integer.parseInt(e[X]);
-            final int y = Integer.parseInt(e[Y]);
-            final Point2d pos = new Point2d(x, y);
-            final Vector2d vec = new Vector2d(0, y);
-            final Hitbox hitbox = new HitboxImpl(5.0, 5.0, pos);
-            moneyList.add(new Money(pos, vec, hitbox));
-        });
-        return moneyList;
-    }
 }
